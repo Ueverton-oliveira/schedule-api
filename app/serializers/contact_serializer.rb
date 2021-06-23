a0c862bb4a557a93602448e60d37eb0bfb924029
Ueverton-oliveira <ueverton.souz@gmail.com>
2021-06-23 08:00:06 -0400

    add information extra

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -5,6 +5,10 @@ class ContactSerializer < ActiveModel::Serializer
   has_many :phones
   has_one :address
 
+  meta do
+    { author: "Ueverton"}
+  end
+
   def attributes(*args)
     h = super(*args)
     #pr-BR --> h[:birthdate] = (I18n.l(object.birthdate) unless object.birthdate.blank?) 
