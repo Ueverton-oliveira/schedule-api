cfb90758015c96a23f52a6d4741e9f990069fc51
Ueverton-oliveira <ueverton.souz@gmail.com>
2021-06-22 20:10:36 -0400

    add date format BR

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,9 +1,14 @@
 class Contact < ApplicationRecord
   belongs_to :kind, optional: true
 
-  def birthdate_br
-    I18n.l(self.birthdate) unless self.birthdate.blank?
+  def to_br
+    { 
+      name: self.name, 
+      email: self.email,
+      birthdate: (I18n.l(self.birthdate) unless self.birthdate.blank?) 
+    }
   end
+
 =begin
   def author
     "Ueverton Oliveira"
