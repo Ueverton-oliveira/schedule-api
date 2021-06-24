f237abecdfb1ed0461b8eefaabc8b495175e4d0e
Ueverton-oliveira <ueverton.souz@gmail.com>
2021-06-23 20:55:24 -0400

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :kinds
-  resources :contacts
+  resources :contacts do
+    resource :kinds, only: [:show]
+    resource :kinds, only: [:show], path: 'relationships/kind'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
